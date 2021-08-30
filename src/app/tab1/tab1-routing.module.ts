fa38815701ac988c13407a5c4912041106c15d92
Eduardo Augusto Zampieri <eduardozampierih@outlook.com>
2021-08-30 08:25:06 -0300

    Projetin

--- src/app/tab1/tab1-routing.module.ts
+++ src/app/tab1/tab1-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Tab1Page } from './tab1.page';
 
@@ -13,4 +13,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
+
 export class Tab1PageRoutingModule {}
