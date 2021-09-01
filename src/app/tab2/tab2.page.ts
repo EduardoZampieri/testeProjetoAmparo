592421c2397cc883537ba29bbd6b2b5b7c21890a
Eduardo Augusto Zampieri <eduardozampierih@outlook.com>
2021-09-01 08:32:22 -0300

    adicionei uma lista com o horario

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Calendar } from '@ionic-native/calendar/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
 
