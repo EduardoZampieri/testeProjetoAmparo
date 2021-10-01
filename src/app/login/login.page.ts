10e235ccf597bd359894ba398523c208c12b97be
Eduardo Augusto Zampieri <eduardozampierih@outlook.com>
2021-10-01 13:56:50 -0300

    fiz alguns retoques na tela de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  email: string;
+  senha: string;
+
+  constructor(public toastController: ToastController, private route: Router) { }
 
   ngOnInit() {
+  }
+
+  login(){
+    if(this.email === 'admin@admin.com' && this.senha === 'admin'){
+      this.route.navigateByUrl('/tabs/tab1');
+      this.presentToast('Bem vindo!', 'success');
+    }else{
+      this.presentToast('Erro! E-mail e/ou senha inválidos!', 'danger');
+    }
+  }
+
+  async presentToast(texto: string, cor: string) {
+    const toast = await this.toastController.create({
+      message: texto,
+      color: cor,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
